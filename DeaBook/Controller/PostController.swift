038e540e861cdb93468df9aaa1152dd32b35cbd5
Dea-loC <dealoc@gmail.com>
2018-04-09 17:24:46 +0200

    Ajout Post Controller

---
+++ DeaBook/Controller/PostController.swift
@@ -0,0 +1,31 @@
+//
+//  PostController.swift
+//  DeaBook
+//
+//  Created by Dea-loC on 09/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class PostController: UIViewController {
+
+    @IBOutlet weak var photoDuPost: UIImageView!
+    @IBOutlet weak var texteDuPost: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func photoAction(_ sender: Any) {
+    }
+    
+    @IBAction func librairieAction(_ sender: Any) {
+    }
+    
+    @IBAction func envoyerPostDansBDD(_ sender: Any) {
+    }
+    
+}
---
+++ DeaBook/View/VueDeaBook.swift
@@ -0,0 +1,30 @@
+//
+//  VueDeaBook.swift
+//  DeaBook
+//
+//  Created by Dea-loC on 09/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class VueDeaBook: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        miseEnPlace()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        miseEnPlace()
+    }
+    
+    func miseEnPlace() {
+        backgroundColor = .white
+        layer.mep(10)
+        
+        
+    }
+
+}
