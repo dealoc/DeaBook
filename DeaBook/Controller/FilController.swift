58efcf35d55bc42491452c95995bf5ffc54f3325
Dea-loC <dealoc@gmail.com>
2018-04-10 11:42:23 +0200

    Premier affichage des posts

--- DeaBook/Controller/FilController.swift
+++ DeaBook/Controller/FilController.swift
@@ -8,15 +8,51 @@
 
 import UIKit
 
-class FilController: UIViewController {
+class FilController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        oberserPosts()
+    }
+    
+    func oberserPosts() {
+        let ref = Refs.obtenir.basePost
+        ref.observe(.childAdded) { (snapshot) in
+            if let postDict = snapshot.value as? [String: AnyObject] {
+                if let userId = postDict[USER_ID] as? String {
+                    Refs.obtenir.baseUtilisateur.child(userId).observe(.value, with: { (userSnap) in
+                        if let userDict = userSnap.value as? [String: String], let prenom = userDict[PRENOM], let nom = userDict[NOM] {
+                            let utilisateur = Utilisateur(id: userSnap.key, prenom: prenom, nom: nom, mail: nil, pays: nil, ville: nil, imageUrl: userDict[IMAGE_URL])
+                            if let date = postDict[DATE_POST] as? Double, let texte = postDict[TEXTE] as? String{
+                                let nouveauPost = Post(id: snapshot.key, date: date, texte: texte, imageUrl: postDict[IMAGE_URL] as? String, utilisateur: utilisateur)
+                                self.posts.append(nouveauPost)
+                                self.tableView.reloadData()
+                            }
+                        }
+                    })
+                }
+            }
+        }
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID) as? PostCell {
+            cell.miseEnPlace(post: posts[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
     
 
 }
---
+++ DeaBook/View/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostCell.swift
+//  DeaBook
+//
+//  Created by Dea-loC on 10/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+
+    @IBOutlet weak var photoDeProfil: ImageRonde!
+    @IBOutlet weak var nomEtPrenom: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageDuPost: UIImageView!
+    @IBOutlet weak var texteDuPost: UILabel!
+    @IBOutlet weak var boutonLike: UIButton!
+    @IBOutlet weak var nombreDeLike: UILabel!
+    
+    var post: Post!
+    
+    func miseEnPlace(post: Post) {
+        self.post = post
+        photoDeProfil.telecharger(self.post.utilisateur.imageUrl)
+        nomEtPrenom.text = self.post.utilisateur.prenom + "  " + self.post.utilisateur.nom
+        imageDuPost.telecharger(self.post.imageUrl)
+        texteDuPost.text = self.post.texte
+        
+    }
+    
+    @IBAction func likeAppuyer(_ sender: Any) {
+    }
+}
